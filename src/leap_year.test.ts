3c42ff407919521b9fb6224ab43c7c969f87ec70
PongDev <pongwiwat.dev@gmail.com>
2024-02-12 12:41:06 +0700

    add jest unit test

---
+++ src/leap_year.test.ts
@@ -0,0 +1,17 @@
+import { isLeapYear } from "./leap_year";
+
+test("Year 1900 is not a leap year", () => {
+  expect(isLeapYear(1900)).toBe(false);
+});
+test("Year 2000 is a leap year", () => {
+  expect(isLeapYear(2000)).toBe(true);
+});
+test("Year 2020 is a leap year", () => {
+  expect(isLeapYear(2020)).toBe(true);
+});
+test("Year 2023 is not a leap year", () => {
+  expect(isLeapYear(2023)).toBe(false);
+});
+test("Year 2024 is a leap year", () => {
+  expect(isLeapYear(2024)).toBe(true);
+});
