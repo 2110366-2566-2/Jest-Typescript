f964011fcb086716c158dfcc19c3761fa77e0392
RefinXD <The32069@gmail.com>
2024-02-13 08:29:46 +0700

    added exercises

--- src/leap_year.ts
+++ src/leap_year.ts
@@ -1,7 +1,7 @@
 export const isLeapYear = (year: number): boolean => {
-  // if (year < 0){
-  //   return false
-  // }
+  if (year < 0){
+    return false
+  }
   if (year % 4 == 0) {
     if (year % 100 == 0) {
       if (year % 400 == 0) {
